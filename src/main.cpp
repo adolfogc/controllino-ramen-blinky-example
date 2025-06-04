e923d153ddb3f61ae86b38b683e0aa56cb0589b4
Adolfo E. García <aegarcia@cerbo.co>
2025-06-04 16:23:43 -0600

    update example

--- src/main.cpp
+++ src/main.cpp
@@ -7,13 +7,16 @@
 TimerActor timer;
 BlinkyLedActor led1(CONTROLLINO_D0, 500); // 500ms interval
 BlinkyLedActor led2(CONTROLLINO_D1, 5000); // 5s interval
+BlinkyLedActor led3(CONTROLLINO_D2, 500);
 
 void setup() {    
     led1.request_timeout_evt >> timer.arm_timeout_evt;
     led2.request_timeout_evt >> timer.arm_timeout_evt;
+    led3.request_timeout_evt >> timer.arm_timeout_evt;
 
     led1.start();
     led2.start();
+    led3.start();
 }
 
 void loop() {
